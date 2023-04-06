33bf5d066e4214c5d52c6750ab64acdb170a266a
Marcos <marcoswodbarros@outlook.com>
2023-04-06 12:57:07 -0300

    Refactoring js (media queries)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Variables
 let firstShoes = document.querySelector('.shoes__pictures--one');
 let secondShoes = document.querySelector('.shoes__pictures--two');
 let thirdShoes = document.querySelector('.shoes__pictures--three');
@@ -9,38 +10,93 @@ let firstBig = document.querySelector('.pictures__one--big');
 let secondBig = document.querySelector('.pictures__two--big');
 let thirdBig = document.querySelector('.pictures__three--big');
 
+let tablet = window.matchMedia("(max-width:1023px");
+let laptop = window.matchMedia("(min-width:1024px)");
 
-firstShoes.addEventListener("click", () => {
-    x.style.display = "inline";
-    x.style.color = "black";
-    bigger.style.display = "none";
-    firstBig.style.display = "block";
-    secondBig.style.display = "none";
-    thirdBig.style.display = "none";
-});
 
-secondShoes.addEventListener("click", () => {
-    x.style.display = "inline";
-    x.style.color = "black";
-    bigger.style.display = "none";
-    firstBig.style.display = "none";
-    secondBig.style.display = "block";
-    thirdBig.style.display = "none";
-});
+// Events
+firstShoes.addEventListener("click", firstShoesOn);
+secondShoes.addEventListener("click", secondShoesOn);
+thirdShoes.addEventListener("click", thirdShoesOn);
+x.addEventListener("click", noX);
+laptop.addEventListener('change', firstShoesOn, secondShoesOn, thirdShoesOn)
 
-thirdShoes.addEventListener("click", () => {
-    x.style.display = "inline";
-    x.style.color = "white";
-    bigger.style.display = "none";
-    firstBig.style.display = "none";
-    secondBig.style.display = "none";
-    thirdBig.style.display = "block";
-});
 
-x.addEventListener("click", () => {
-    x.style.display = "none";
-    bigger.style.display = "block";
-    firstBig.style.display = "none";
-    secondBig.style.display = "none";
-    thirdBig.style.display = "none";
-})
+// Functions
+function firstShoesOn() {
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        x.style.display = "inline";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "block";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+}
+
+function secondShoesOn() {
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        x.style.display = "inline";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "block";
+        thirdBig.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+}
+
+function thirdShoesOn() {
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        x.style.display = "inline";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "block";
+    }
+
+    else {
+        x.style.display = "none";
+        x.style.color = "black";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+}
+
+function noX() {
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        x.style.display = "none";
+        bigger.style.display = "block";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+        bigger.style.display = "none";
+        firstBig.style.display = "none";
+        secondBig.style.display = "none";
+        thirdBig.style.display = "none";
+    }
+}
