8f404ea3cb90ba9cb3cedd3e99c13012648d4a3f
Marcos <marcoswodbarros@outlook.com>
2023-04-06 10:47:41 -0300

    Updating js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ let firstShoes = document.querySelector('.shoes__pictures--one');
 let secondShoes = document.querySelector('.shoes__pictures--two');
 let thirdShoes = document.querySelector('.shoes__pictures--three');
 
+let x = document.querySelector('.fa-circle-xmark');
 let bigger = document.querySelector('.big__shoes');
 
 let firstBig = document.querySelector('.pictures__one--big');
@@ -10,6 +11,8 @@ let thirdBig = document.querySelector('.pictures__three--big');
 
 
 firstShoes.addEventListener("click", () => {
+    x.style.display = "inline";
+    x.style.color = "black";
     bigger.style.display = "none";
     firstBig.style.display = "block";
     secondBig.style.display = "none";
@@ -17,6 +20,8 @@ firstShoes.addEventListener("click", () => {
 });
 
 secondShoes.addEventListener("click", () => {
+    x.style.display = "inline";
+    x.style.color = "black";
     bigger.style.display = "none";
     firstBig.style.display = "none";
     secondBig.style.display = "block";
@@ -24,8 +29,18 @@ secondShoes.addEventListener("click", () => {
 });
 
 thirdShoes.addEventListener("click", () => {
+    x.style.display = "inline";
+    x.style.color = "white";
     bigger.style.display = "none";
     firstBig.style.display = "none";
     secondBig.style.display = "none";
     thirdBig.style.display = "block";
 });
+
+x.addEventListener("click", () => {
+    x.style.display = "none";
+    bigger.style.display = "block";
+    firstBig.style.display = "none";
+    secondBig.style.display = "none";
+    thirdBig.style.display = "none";
+})
